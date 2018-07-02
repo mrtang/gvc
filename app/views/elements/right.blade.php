bebb01dba9b1ae9da2f7a0fbcb5d1be2fbe54634
Tang Hoang Anh <anhth@vnptepay.com.vn>
2018-07-02 16:24:27 +0700

    right sb

--- app/views/elements/right.blade.php
+++ app/views/elements/right.blade.php
@@ -1,34 +1,18 @@
 <div class="side-right">
-                <h3 class="side-title">Các tin khác</h3>
-                <div class="list-other-news">
-                        <div class="other-news">
-                            <a href="https://fo4.garena.vn/kich-hoat-team-color-trong-fifa-online-4/">
-                                <img class="news-image" src="https://1.bp.blogspot.com/-iJVK6pgONZg/WwtxsNLy7xI/AAAAAAAAAYE/2YnqdNxt86omCb2jugUK2NsfGJ2Ud-aNACLcBGAs/s1600/team-color-trong-fifa-online-4-3.jpg" alt="">
-                            </a>
-                            <a href="https://fo4.garena.vn/kich-hoat-team-color-trong-fifa-online-4/">
-                                <h4 class="news-title">Kích hoạt Team Color trong FIFA Online 4</h4>
-                            </a>
-                            <div class="news-time">09h37 - 30/05/2018</div>
-                            <div class="news-caption">
-                                Gửi huấn luyện viên FIFA Online 4,
-
-Ban huấn luyện đội bóng xin gửi HLV cách thức kích hoạt Team Color trong FIFA Online 4.                            </div>
-                        </div>
-              
-                    <div class="other-news">
-                        <a href="">
-                            <img class="news-image" src="https://cdn.vn.garenanow.com/web/fo3/fo4/home-page/images/news-caption-1.png" alt="">
-                        </a>
-                        <a href="">
-                           <h4 class="news-title">Tên tin tức mới 01</h4>
-                     </a>
-                   <div class="news-time">12h00 - 16/02/2017</div>
-                    <div class="news-caption">
-                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos est laboriosam magnam
-                            mollitia nisi nulla officia sed sit veritatis? A assumenda, fuga itaque laborum officiis
-                          optio
-                          possimus quaerat quis tempore.
-                       </div>
-                   </div>
-                </div>
+    <h3 class="side-title">Các tin khác</h3>
+    <div class="list-other-news">
+        @foreach(CommonHelper::getNoticeNew() as $notice)
+            <div class="other-news">
+                <?php $imageUrl = Media::find($notice->media_id); ?>
+                <a href="{{ URL::to('/tin-tuc/'.$notice->new_id.'/'.$notice->slug.'.html') }}">
+                    <img class="news-image" src="{{ $imageUrl->path.$imageUrl->original }}" alt="">
+                </a>
+                <a href="{{ URL::to('/tin-tuc/'.$notice->new_id.'/'.$notice->slug.'.html') }}">
+                    <h4 class="news-title">{{$notice->title}}</h4>
+                </a>
+                <div class="news-time">{{date('H',strtotime($notice->created_at))}}h{{date('i',strtotime($notice->created_at))}} - {{date('d/m/Y',strtotime($notice->created_at))}}</div>
+                <div class="news-caption">{{$notice->summary}}</div>
             </div>
+        @endforeach
+    </div>
+</div>
